728c25870e9bd1b8a06dca9be5028e1b1e9f2724
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-01-14 14:07:44 +0100

    feat(profile_page): add custom app bar for profile page

---
+++ lib/components/profile_page_app_bar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_shadow/flutter_inset_shadow.dart';
+
+class ProfilePageAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const ProfilePageAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: kToolbarHeight + 35,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(25),
+            spreadRadius: 0,
+            blurRadius: 4,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            IconButton(
+              onPressed: () => Navigator.pop(context),
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.black),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                ),
+              ),
+              color: Colors.white,
+              icon: ImageIcon(
+                AssetImage("assets/icons/left_arrow.png"),
+                size: 20,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.black),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                ),
+              ),
+              color: Colors.white,
+              icon: ImageIcon(
+                AssetImage("assets/icons/dots_horizontal.png"),
+                size: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}

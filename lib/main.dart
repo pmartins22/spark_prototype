e6c246ae45e9de85a333cd9c3c6b2ec103cfa997
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-03-18 14:00:31 +0100

    feat: integrate Riverpod with main application

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,21 @@ import 'package:spark_prototype/splash_screen.dart';
 import 'map_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     WidgetsFlutterBinding.ensureInitialized();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp
-    ]);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       routes: {
         '/home': (context) => Menu(),
         '/profile': (context) => ProfilePage(),
